96788dcd57bae52533d4e402859247c2f58da071
yevgenykuz <yevgenykuz@users.noreply.github.com>
2018-01-15 00:56:40 +0200

    Created wiki app basic models + admin views

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,3 +1,26 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from wiki.models import CustomFieldType, CustomField, Section, Entry
+
+
+@admin.register(CustomFieldType)
+class CustomFieldTypeAdmin(admin.ModelAdmin):
+    list_display = ["name", "description"]
+
+
+@admin.register(CustomField)
+class CustomFieldAdmin(admin.ModelAdmin):
+    list_display = ["name", "type"]
+    list_filter = ["type"]
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ["name", "description"]
+
+
+@admin.register(Entry)
+class EntryAdmin(admin.ModelAdmin):
+    list_display = ["name", "section", "published"]
+    list_filter = ["section", "published"]
+    filter_horizontal = ["custom_fields_presentation_order", "custom_fields"]
--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,83 @@
-# from django.db import models
+from ckeditor.fields import RichTextField
+from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class CustomFieldType(models.Model):
+    name = models.CharField(max_length=200, unique=True, help_text="A custom field type")
+    slug = models.SlugField(max_length=255, unique=True, editable=False)
+    name_plural = models.CharField(max_length=255, help_text="This type in plural, for wiki entry presentation")
+    description = models.TextField(max_length=2500, help_text="Describe this custom field type")
+
+    class Meta:
+        ordering = ["slug"]
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        # update slug field when saving from admin:
+        self.slug = slugify(self.name)
+        super(CustomFieldType, self).save(*args, **kwargs)
+
+
+class CustomField(models.Model):
+    name = models.CharField(max_length=200, help_text="A custom field")
+    type = models.ForeignKey(CustomFieldType, on_delete=models.SET_NULL, null=True, help_text="The type of this field")
+    slug = models.SlugField(max_length=255, unique=True, editable=False)
+    value = RichTextField(max_length=20000, help_text="The value of this field")
+
+    class Meta:
+        ordering = ["type__slug", "slug"]
+
+    def __str__(self):
+        return "[" + self.type.name + "] " + self.name
+
+    def save(self, *args, **kwargs):
+        # update slug field when saving from admin:
+        self.slug = self.type.slug + "-" + slugify(self.name)
+        super(CustomField, self).save(*args, **kwargs)
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=200, unique=True, help_text="A wiki section/category")
+    slug = models.SlugField(max_length=255, unique=True, editable=False)
+    description = models.TextField(max_length=2500, help_text="Describe this wiki section")
+
+    class Meta:
+        ordering = ["slug"]
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        # update slug field when saving from admin:
+        self.slug = slugify(self.name)
+        super(Section, self).save(*args, **kwargs)
+
+
+class Entry(models.Model):
+    section = models.ForeignKey(Section, on_delete=models.SET_NULL, null=True,
+                                help_text="The section to witch this entry belongs")
+    name = models.CharField(max_length=200, help_text="A wiki entry")
+    slug = models.SlugField(max_length=255, unique=True, editable=False)
+    published = models.BooleanField(default=False)
+    value = RichTextField(max_length=20000, help_text="The value of this field", blank=True)
+    custom_fields_presentation_order = models.ManyToManyField(CustomFieldType, blank=True,
+                                                              help_text="The custom field types (if any), "
+                                                                        "the order is the presentation order")
+    custom_fields = models.ManyToManyField(CustomField, blank=True,
+                                           help_text="The custom fields (if any), "
+                                                     "the order is the presentation order")
+
+    class Meta:
+        ordering = ["section__slug", "slug"]
+        verbose_name_plural = "entries"
+
+    def __str__(self):
+        return "[" + self.section.name + "] " + self.name
+
+    def save(self, *args, **kwargs):
+        # update slug field when saving from admin:
+        self.slug = self.section.slug + "-" + slugify(self.name)
+        super(Entry, self).save(*args, **kwargs)
